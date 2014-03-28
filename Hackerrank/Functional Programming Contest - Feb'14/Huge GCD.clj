218dd912e99a02b175b8e1dce9c43d6554724952
Shimi Zhang <zhangshimi@gmail.com>
2014-03-28 03:06:00 -0500

    huge gcd

---
+++ Hackerrank/Functional Programming Contest - Feb'14/Huge GCD.clj
@@ -0,0 +1,7 @@
+(let [parse (fn [x] (map #(Integer. %) (clojure.string/split x #" ")))
+  	  lines (line-seq (java.io.BufferedReader. *in*))
+	  gcd (fn [a b] (if (zero? b) a (recur b (mod a b))))
+	  [l1 l2 l3 l4] lines
+	  a (reduce * 1N (parse l2))
+	  b (reduce * 1N (parse l4))]
+(println (int (mod (gcd a b) 1000000007))))
