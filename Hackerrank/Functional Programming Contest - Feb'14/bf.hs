27447837628186bf88c4ee0fc870e662f828b85b
Shimi Zhang <zhangshimi@gmail.com>
2014-04-16 10:35:31 -0500

    fixed

--- Hackerrank/Functional Programming Contest - Feb'14/bf.hs
+++ Hackerrank/Functional Programming Contest - Feb'14/bf.hs
@@ -24,14 +24,13 @@ putMp :: Int -> Int -> HM.HashMap Int Int -> HM.HashMap Int Int
 putMp idx val mp = HM.insert idx val mp
 
 ipt:: Int -> Int -> HM.HashMap Int Int -> [Char] -> HM.HashMap Int Int -> Int -> [Char] -> IO()
-ipt cIdx mIdx mem code jump step stream@(x:xs)
-  | step >= 100000 = (putStr "\nPROCESS TIME OUT. KILLED!!!")
-  | cIdx == length code = return ()
-  | cmd == ',' = ipt (cIdx+1) mIdx (putMp mIdx (ord x) mem) code jump nstep xs
+ipt cIdx mIdx mem code jump step stream@(x:xs)  
+  | cIdx >= length code = return ()
+  | step == 100000 = (putStr "\nPROCESS TIME OUT. KILLED!!!")
   | cmd == '+' = ipt (cIdx+1) mIdx (putMp mIdx (((getMp mIdx mem)+1)`rem`256) mem) code jump nstep stream
   | cmd == '-' = ipt (cIdx+1) mIdx (putMp mIdx (((getMp mIdx mem)-1+256)`rem`256) mem) code jump nstep stream
-  | cmd == '.' = (putChar. chr $ HM.lookupDefault 0 mIdx mem) >> hFlush stdout >>
-                 ipt (cIdx+1) mIdx mem code jump nstep stream
+  | cmd == '.' = (putChar. chr $ getMp mIdx mem) >> hFlush stdout >> ipt (cIdx+1) mIdx mem code jump nstep stream
+  | cmd == ',' = ipt (cIdx+1) mIdx (putMp mIdx (ord x) mem) code jump nstep xs
   | cmd == '>' = ipt (cIdx+1) (mIdx+1) mem code jump nstep stream
   | cmd == '<' = ipt (cIdx+1) (mIdx-1) mem code jump nstep stream
   | cmd == '[' && (getMp mIdx mem) == 0 = ipt (getMp cIdx jump) mIdx mem code jump nstep stream
@@ -42,10 +41,9 @@ ipt cIdx mIdx mem code jump step stream@(x:xs)
     cmd = code!!cIdx
     nstep = step + 1
 
-main :: IO ()
 main = do
-  [n, m] <- getArray
-  stream <- getLine
+  [n, m]  <- getArray
+  stream  <- getLine
   content <- getContents
   let
     code = concat. map parse. lines $ content
